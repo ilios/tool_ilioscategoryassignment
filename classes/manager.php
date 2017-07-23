2d6f7c3deab23749e5ade8e5bb78cb018b1f25df
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-07-22 22:34:39 -0700

    implement sync job deletion.

--- classes/manager.php
+++ classes/manager.php
@@ -2,6 +2,10 @@
 
 namespace tool_ilioscategoryassignment;
 
+/* @global $CFG */
+require_once($CFG->libdir . '/coursecatlib.php');
+require_once($CFG->libdir . '/accesslib.php');
+
 /**
  * Sync job definition model.
  *
@@ -10,11 +14,19 @@ namespace tool_ilioscategoryassignment;
  */
 class manager {
 
+    /**
+     * @param $id
+     * @return sync_job|null
+     */
     public static function get_sync_job($id) {
         $jobs = self::get_sync_jobs(array('id' => $id));
         return empty($jobs) ? null : $jobs[0];
     }
 
+    /**
+     * @param array $filters
+     * @return sync_job[]
+     */
     public static function get_sync_jobs(array $filters = array()) {
         global $DB;
         $jobs = array();
@@ -43,6 +55,9 @@ class manager {
         return $jobs;
     }
 
+    /**
+     * @param int $job_id
+     */
     public static function disable_job($job_id) {
         global $DB;
         $table_name = 'tool_ilioscatassignment';
@@ -53,6 +68,9 @@ class manager {
         }
     }
 
+    /**
+     * @param int $job_id
+     */
     public static function enable_job($job_id) {
         global $DB;
         $table_name = 'tool_ilioscatassignment';
@@ -63,8 +81,26 @@ class manager {
         }
     }
 
+    /**
+     * @param int $job_id
+     */
     public static function delete_job($job_id) {
         global $DB;
-        // @todo implement [ST 2017/07/21]
+        $job = self::get_sync_job($job_id);
+        if (empty($job)) {
+            return;
+        }
+
+        // delete the given job
+        $DB->delete_records('tool_ilioscatassignment_src', array('jobid' => $job_id));
+        $DB->delete_records('tool_ilioscatassignment', array('id' => $job_id));
+
+        // remove any course category role assignments that were managed by the given job
+        $category = \coursecat::get($job->get_course_category_id(), IGNORE_MISSING);
+        if (empty($category)) {
+            return;
+        }
+        $context = $category->get_context();
+        role_unassign_all(array('component' => 'tool_ilioscategoryassignment', 'roleid' => $job->get_role_id(), 'contextid' => $context->id));
     }
 }
