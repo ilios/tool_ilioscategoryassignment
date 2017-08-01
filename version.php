f1658d26be8b79a07a0b3e9973c27eac19059397
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2017-08-01 14:40:04 -0700

    Update version.php
    version and release bump.

--- version.php
+++ version.php
@@ -7,11 +7,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017072800;
+$plugin->version = 2017080100;
 $plugin->requires = 2016112900;
 $plugin->component = 'tool_ilioscategoryassignment';
-$plugin->maturity = MATURITY_RC;
-$plugin->release = '1.0.0-RC2';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
 $plugin->dependencies = array(
     'local_iliosapiclient' => 2017071700,
 );
