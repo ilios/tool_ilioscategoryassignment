3c07e772a04e7d330e04556c1bf80113bd9e48d0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-12-12 14:58:52 -0800

    version and requirement bump to Moodle 4.1

--- version.php
+++ version.php
@@ -7,10 +7,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022103100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2022041200;        // Requires this Moodle version
+$plugin->version   = 2022121200;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022111800;        // Requires this Moodle version
 $plugin->component = 'tool_ilioscategoryassignment';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_iliosapiclient' => 2022103100,
+    'local_iliosapiclient' => 2022121200,
 );
