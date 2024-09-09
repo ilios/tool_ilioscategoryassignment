b4b46f68ce5f849afffdbb889a40d1556c5b5813
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-09-09 15:00:31 -0700

    re-implements privacy provider.
    the null provider doesn't cut it anymore, since we're tracking edits
    to sync jobs in tool_ilioscategoryassignment::usermodified.
    apart from declaring it as metadata, there's nothing actionable here,
    therefore most methods remain unimplemented.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Privacy Subsystem implementation for filter_ucsfezproxy.
+ * Privacy Subsystem implementation for tool_ilioscategoryassignment.
  *
  * @package    tool_ilioscategoryassignment
  * @copyright  The Regents of the University of California
@@ -23,20 +23,97 @@
  */
 namespace tool_ilioscategoryassignment\privacy;
 
-use core_privacy\local\metadata\null_provider;
+
+use context;
+use core_privacy\local\metadata\collection;
+use core_privacy\local\metadata\provider as metadata_provider;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\core_userlist_provider;
+use core_privacy\local\request\plugin\provider as data_provider;
+use core_privacy\local\request\userlist;
+
 /**
- * Privacy Subsystem for tool_ilioscategoryassignment implementing null_provider.
+ * Privacy Subsystem for tool_ilioscategoryassignment.
  *
  * @package    tool_ilioscategoryassignment
  * @copyright  The Regents of the University of California
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements null_provider {
+class provider implements core_userlist_provider, data_provider, metadata_provider {
+
 
     /**
-     * {@inheritdoc}
+     * Returns the metadata for the user data stored in this plugin.
+     *
+     * @param collection $collection The metadata collection to add items to.
+     * @return collection The updated metadata collection.
      */
-    public static function get_reason(): string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_database_table(
+            'tool_ilioscategoryassignment',
+            [
+                'usermodified' => 'privacy:metadata:usermodified',
+            ],
+            'privacy:metadata:tool_ilioscategoryassignment',
+        );
+        return $collection;
+    }
+
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid The user to search.
+     * @return contextlist $contextlist The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist {
+        // Return an empty context list. There are no contexts for the given user.
+        return new contextlist();
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist): void {
+        // Not implemented. There are no users in any given context.
+    }
+
+    /**
+     * Delete all data for all users in the specified context.
+     *
+     * @param context $context The specific context to delete data for.
+     */
+    public static function delete_data_for_all_users_in_context(context $context): void {
+        // Not implemented. There is no context that would allow deletion.
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
+     */
+    public static function delete_data_for_user(approved_contextlist $contextlist): void {
+        // Not implemented. There is no context that would allow deletion.
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist): void {
+        // Not implemented. There is no context that would allow deletion.
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist): void {
+        // Not implemented. There is nothing to export here.
     }
 }
