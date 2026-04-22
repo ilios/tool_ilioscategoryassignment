377f85b8e3a9f7f938648caf5d4a5dbdd766c004
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-04-22 16:15:06 -0700

    version bump.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026022300;       // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026022301;       // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2025092600;       // Requires this Moodle version.
 $plugin->component = 'tool_ilioscategoryassignment';     // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v5.1';
+$plugin->release = 'v5.1.1';
 $plugin->supported = [501, 501];
 $plugin->maturity = MATURITY_STABLE;
